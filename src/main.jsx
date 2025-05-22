286f1a2b8b46af2cb8c9a9fb88fa5a9916c4c315
iversson-estudos <i versson.estudos@gmail.com>
2025-05-22 10:36:00 -0300

    cleaning

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import getRandomCard from './utils/getRandomCard'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
