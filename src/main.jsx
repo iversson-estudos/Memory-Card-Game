714d78d8aa189466859e483b5b67e139b018276b
iversson-estudos <i versson.estudos@gmail.com>
2025-05-22 10:35:37 -0300

    downloaded the imgs, api doesnt allow cors :(

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import getRandomCard from './utils/getRandomCard'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <p>GG</p>
   </StrictMode>
 )
+
+getRandomCard();
