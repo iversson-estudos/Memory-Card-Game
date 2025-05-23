662ff722d8f06077b255ef17f075ab000fdbfb41
iversson-estudos <i versson.estudos@gmail.com>
2025-05-23 15:17:46 -0300

    images aint loading on netlify, changed directories

--- src/components/ImgGrid.jsx
+++ src/components/ImgGrid.jsx
@@ -6,7 +6,7 @@ export default function ImgGrid ({imgOnClick,order=[0,1,2,3,4,5,6,7,8,9,10,11,12
         return(
         <div className={styles.imgGrid}>
             {order.map((value)=>(
-                <ButtonWithImg onClick={imgOnClick} key={value} cardID={value} imgUrl={`/src/data/cards/${value}.jpg`}/>
+                <ButtonWithImg onClick={imgOnClick} key={value} cardID={value} imgUrl={`/public/cards/${value}.jpg`}/>
             ))}
         </div>
     )
